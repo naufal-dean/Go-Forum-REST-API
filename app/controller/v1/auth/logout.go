946cd888782b2fdcb7d94686e0c3d7f89f2af775
naufal-dean <naufal.dean@gmail.com>
2021-01-09 20:51:57 +0700

    refactor: get claims from context in logout handler

--- app/controller/v1/auth/logout.go
+++ app/controller/v1/auth/logout.go
@@ -1,59 +1,25 @@
 package auth
 
 import (
-	"fmt"
-	"github.com/dgrijalva/jwt-go"
 	"github.com/naufal-dean/onboarding-dean-local/app/core"
 	"github.com/naufal-dean/onboarding-dean-local/app/lib/auth"
 	"github.com/naufal-dean/onboarding-dean-local/app/model/orm"
 	"github.com/naufal-dean/onboarding-dean-local/app/response"
+	"github.com/pkg/errors"
 	"net/http"
-	"os"
-	"strings"
 )
 
 func Logout(a *core.App) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Get token
-		authorizationHeader := r.Header.Get("Authorization")
-		arr := strings.Split(authorizationHeader, " ")
-		tokenString := ""
-		if len(arr) == 2 && arr[0] == "Bearer" {
-			tokenString = arr[1]
-		} else {
-			// TODO: create response object
-			response.JSON(w, http.StatusUnauthorized, "Invalid token")
-			return
-		}
-
-		// Parse token
-		token, err := jwt.ParseWithClaims(tokenString, &auth.TokenClaims{}, func(token *jwt.Token) (interface{}, error) {
-			// Check token signing method
-			method, ok := token.Method.(*jwt.SigningMethodHMAC)
-			if !ok || method != jwt.SigningMethodHS256 {
-				// TODO: create response object
-				return nil, fmt.Errorf("invalid token signing method")
-			}
-			// Return
-			return []byte(os.Getenv("APP_SECRET")), nil
-		})
-		if err != nil {
-			// TODO: create response object
-			response.JSON(w, http.StatusUnauthorized, err.Error())
-			return
-		}
-
-		// Check claims
-		claims, ok := token.Claims.(*auth.TokenClaims)
-		if !ok || !token.Valid {
-			// TODO: create response object
-			response.JSON(w, http.StatusUnauthorized, "Invalid token claims")
-			return
+		// Get claims context
+		claims, ok := r.Context().Value("claims").(*auth.TokenClaims)
+		if !ok {
+			panic(errors.New("invalid claims context"))
 		}
 
 		// Delete token from table
 		ormToken := &orm.Token{}
-		err = a.DB.Where("user_id = ? AND token_uuid = ?", claims.UserID, claims.TokenUUID).First(&ormToken).Error
+		err := a.DB.Where("user_id = ? AND token_uuid = ?", claims.UserID, claims.TokenUUID).First(&ormToken).Error
 		if err != nil {
 			response.JSON(w, http.StatusUnauthorized, "Invalid token value last")
 			return
