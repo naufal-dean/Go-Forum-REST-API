a1eff59132e871cd787c7fa1e5ac9107f3285a04
naufal.dean <naufal.dean@pinhome.id>
2021-01-14 18:32:00 +0700

    chore: remove deleted at field so it can be hard deleted

--- app/model/orm/base.go
+++ app/model/orm/base.go
@@ -1,7 +1,6 @@
 package orm
 
 import (
-	"gorm.io/gorm"
 	"time"
 )
 
@@ -9,5 +8,4 @@ type BaseModel struct {
 	ID        uint           `gorm:"primaryKey" json:"id"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
