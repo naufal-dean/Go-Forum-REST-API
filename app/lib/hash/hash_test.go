69de91a30c455c30e4cf027f9190b81e9833cf02
naufal.dean <naufal.dean@pinhome.id>
2021-01-14 18:37:15 +0700

    test: update lib hash test

--- app/lib/hash/hash_test.go
+++ app/lib/hash/hash_test.go
@@ -1,10 +1,13 @@
 package hash
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 var makePasswordHashTests = []struct {
 	raw string
-} {
+}{
 	{"password"},
 	{"1234567890"},
 	{"_!@#$*&^"},
@@ -12,20 +15,17 @@ var makePasswordHashTests = []struct {
 }
 
 func TestMakePasswordHash(t *testing.T) {
-	for i := range makePasswordHashTests {
-		test := &makePasswordHashTests[i]
-		hash := MakePasswordHash(test.raw)
-		if !CheckPasswordHash(test.raw, hash) {
-			t.Errorf("Invalid hash generated")
-		}
+	for _, tc := range makePasswordHashTests {
+		hash := MakePasswordHash(tc.raw)
+		assert.True(t, CheckPasswordHash(tc.raw, hash), "invalid hash generated")
 	}
 }
 
 var checkPasswordHashTests = []struct {
-	raw string
+	raw  string
 	hash string
-	out bool
-} {
+	out  bool
+}{
 	{"password", MakePasswordHash("password"), true},
 	{"password", MakePasswordHash("passwordz"), false},
 	{"password", "", false},
@@ -34,11 +34,8 @@ var checkPasswordHashTests = []struct {
 }
 
 func TestCheckPasswordHash(t *testing.T) {
-	for i := range checkPasswordHashTests {
-		test := &checkPasswordHashTests[i]
-		out := CheckPasswordHash(test.raw, test.hash)
-		if out != test.out {
-			t.Errorf("Invalid hash check result, out: %v, want: %v", out, test.out)
-		}
+	for _, tc := range checkPasswordHashTests {
+		out := CheckPasswordHash(tc.raw, tc.hash)
+		assert.Equalf(t, tc.out, out, "invalid hash check result, out: %v, want: %v", out, tc.out)
 	}
 }
